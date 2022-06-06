a000022fccde5752a768a6f91be014126f52d446
ariasmartinez <celiaria@hotmail.com>
2022-06-06 14:32:17 +0300

    clean some comments

--- Project/resources/html/insertPet.php
+++ Project/resources/html/insertPet.php
@@ -6,23 +6,18 @@ $type = $_POST['type'];
 $breed = $_POST['breed'];
 $username = $_COOKIE["username"];
 
-echo "1";
+
 $image_dir= './../images/';
 $temp = $_FILES['file']['tmp_name'];
 $name_file = $_FILES['file']['name'];
-if (move_uploaded_file($temp, '$image_dir/$name_file')) {
-    echo "El fichero es válido y se subió con éxito.\n";
-} else {
-    echo "¡Posible ataque de subida de ficheros!\n";
-}
 
 
 
-echo "2";
-$image = $image_dir . $_FILES['file']['name'];
+
+
 $return ="";  
 
-echo "3";
+
 
 
 
@@ -31,7 +26,7 @@ $sql = "INSERT INTO pets (name, username, type, breed, img_dir)
 
 
 if (mysqli_query($dbconn, $sql)) {
-        if (move_uploaded_file($tempname, $folder))  {
+        if (move_uploaded_file($temp, '$image_dir/$name_file'))  {
             $msg = "Image uploaded successfully";
         }else{
             $msg = "Failed to upload image";
